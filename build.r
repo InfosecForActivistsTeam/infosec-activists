780114d845cfd92125d86e140808fb0c6f306d79
Mark Sherman <shermanm@emmanuel.edu>
2021-08-04 17:31:38 -0400

    delete old R build script

--- build.r
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-#
-
-# Might belong in `.Rprofile` instead
-renv::restore()
-
-rmarkdown::render("index.Rmd",
-    output_file="Infosec_for_Activists",
-    output_format=c("pdf_document"))
